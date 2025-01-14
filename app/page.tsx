1d383eab48d8fe06cdc68e2348de6b276b80a6da
lodgepolepines <53026628+lodgepolepines@users.noreply.github.com>
2025-01-13 23:47:23 -0500

    Updates

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import FlashcardGame from './components/FlashcardGame';
-
-export default function Home() {
-  return (
-    <main className="container mx-auto py-8">
-      <FlashcardGame />
-    </main>
-  );
-}
