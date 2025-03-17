1ce3e15645a159f8a4c7f776fe8ef466fa23ab19
lodgepolepines <53026628+lodgepolepines@users.noreply.github.com>
2025-03-16 22:17:58 -0400

    update config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,16 @@
-/** @type {import('next').NextConfig} */
+import type { NextConfig } from "next";
 
 const isProd = process.env.NODE_ENV === 'production';
-const nextConfig = {
+const nextConfig: NextConfig = {
+  output: 'export', // Enable static export
+  trailingSlash: true, // Helps with GitHub Pages routing
   reactStrictMode: true,
   images: {
     unoptimized: true, // Disable default image optimization
-  },
+  },  
+  // Configure basePath for GitHub Pages
   assetPrefix: isProd ? '/chinese-flashcards/' : '',
   basePath: isProd ? '/chinese-flashcards' : '',
-  // output: 'export'
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
